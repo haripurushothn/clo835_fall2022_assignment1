e0c8cf98771706610fe4843b489da1e71e7c6085
haripurushothn <111623234+haripurushothn@users.noreply.github.com>
2022-10-03 00:51:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "passwors"
+DBPWD = os.environ.get("DBPWD") or "pw"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT"))
+DBPORT = 3306
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
